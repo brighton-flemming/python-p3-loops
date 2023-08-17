ce1ea191561eafb09b8aa8905dcb5a0bbc5beade
brighton.flemming <brighton.flemming@student.moringaschool.com>
2023-08-18 02:25:06 +0300

    Mission Passed

--- lib/looping.py
+++ lib/looping.py
@@ -1,8 +1,15 @@
 #!/usr/bin/env python3
 
-def happy_new_year():
-    # code goes here!
-    pass
+def happy_new_year():  
+ num = 10
+ while num > 0 : 
+   print(num)
+   num -= 1 
+   print("Happy New Year!")
+
+happy_new_year()
+
+
 
 def square_integers(int_list):
     # code goes here!
