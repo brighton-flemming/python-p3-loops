840f40269355b83eaee40d80a9c22f1c089c630e
brighton.flemming <brighton.flemming@student.moringaschool.com>
2023-08-18 02:52:20 +0300

    Mission Passed

--- lib/looping.py
+++ lib/looping.py
@@ -21,5 +21,15 @@ square_integers([1, 2, 3, 4, 5])
 square_integers([-1, -2, -3, -4, -5])
 
 def fizzbuzz():
-    # code goes here!
-    pass
+   for num in range(1, 101):
+     if (num % 3 == 0) and (num % 5 == 0):
+       print("FizzBuzz")
+     elif(num % 3 == 0):
+       print("Fizz")
+     elif(num % 5 == 0):
+       print("Buzz")
+     else:
+       print(num)
+     
+
+fizzbuzz()
